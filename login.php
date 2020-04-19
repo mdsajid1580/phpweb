871549ad450d3fee19ea1f03e4c80abfc0778ec9
mdsajid1580 <56562295+mdsajid1580@users.noreply.github.com>
2020-04-19 10:58:05 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Login </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<style> .bg{background:url('../app/image.jpg') no-repeat; width:100%; height:100vh; background-size: cover} .form-design {border:0px solid white; padding:50px 60px 10px 60px; margin-top: 120px;
+	   -webkit-box-shadow: -2px 4px 31px 11px rgba(0,0,0,.75);
+       -moz-box-shadow: -2px 4px 31px 11px rgba(0,0,0,.75);
+        box-shadow: -2px 4px 31px 11px rgba(5,5,5,.75);} .regis{ padding:5px; margin-left: 30px;} </style>
+</head>
+<body>
+<div class="conatiner-fluid bg">
+
+	<div class="row">
+		<div class="com-md-4 col-sm-4 col=xl-12"></div>
+		<div class="com-md-4 col-sm-4 col=xl-12">
+		<form class="form-design">
+			<h4>Login to Ramanasri Institute</h2>
+		  	<div class="form-group">
+
+		    <label for="exampleInputEmail1">Email address</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+		    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		  	</div>
+		    <div class="form-group">
+		    <label for="exampleInputPassword1">Password</label>
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		    </div>
+		    <div class="form-group form-check">
+		    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+		    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+		    </div>
+		    <button type="submit" class="btn btn-primary btn-block">Login	</button>
+		    <br><br>
+		    <p class="text-white"> New to website? <a href=""> <span class=" strong bg-primary text-white regis"> REGISTER </span></a></p>
+		</form>	
+
+		</div>
+		<div class="com-md-4 col-sm-4 col=xl-12"></div>
+	</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
